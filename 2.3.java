de637d5e8e45b2ebac693d3bf159f1f094d11e55
Xichao Wang <w.xichao@gmail.com>
2015-10-18 14:59:38 -0700

    2.3 completed

---
+++ 2.3.java
@@ -0,0 +1,11 @@
+boolean deleteNode(LinkedListNode node){
+	if(n == NULL || n.next = NULL) {
+		return false
+	}
+
+	LinkedListNode next = n.next;
+	n.data = next.data;
+	n.next = n.next.next;
+	return true;
+}
+
