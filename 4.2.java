ab8b3c2c3fa0c73c1ded80ce724484cd5f72b0f1
Xichao Wang <w.xichao@gmail.com>
2015-11-04 18:02:52 -0800

    4.2 completed

---
+++ 4.2.java
@@ -0,0 +1,13 @@
+
+TreeNode createMinimalBST(int array[], int start, int end){
+
+	if(end < start){
+		return null;
+	}
+
+	int mid = (start + end) / 2;
+	TreeNode node = new TreeNode(array[mid]);
+	node.left = createMinimalBST(array[], start, mid - 1);
+	node.right = createMinimalBST(array[], mid + 1, end);
+	return node;
+}
